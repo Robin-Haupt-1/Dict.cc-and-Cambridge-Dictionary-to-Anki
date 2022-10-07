b1b3c0d9ccf725760627124dfa85e40d5bd9f4d1
Robin-Haupt-1 <robinhaupt@outlook.com>
2022-10-07 16:46:34 +0200

    hot reload addon during development

--- __init__.py
+++ __init__.py
@@ -1,29 +1,22 @@
 from aqt.qt import QAction
 from aqt import gui_hooks
+import importlib
+import os
+import sys
+from aqt import mw
 
-from .utils import *
-from .main import ImportEwFromCambridge
-from .constants import EnglishProfile, FrenchProfile, Profile
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+print(sys.path)
+import import_dict_cc
 
-import_task = None
-
-
-def start_import(profile: Profile):
-    global import_task
-    import_task = ImportEwFromCambridge(profile)
-
-
-def init():
-    profiles: [Profile] = [EnglishProfile, FrenchProfile]
-    for profile in profiles:
-        # add menu option to import new cards
-        options_action = QAction(f"Import from Cambridge ({profile.language})...", mw)
-        options_action.triggered.connect(lambda _, o=mw: start_import(profile))
-        mw.form.menuTools.addAction(options_action)
-        # add menu option to import new cards
-        options_action = QAction(f"Update Tampermonkey list ({profile.language})", mw)
-        options_action.triggered.connect(lambda _, o=mw: profile.update_tampermonkey_list())
-        mw.form.menuTools.addAction(options_action)
+# add menu option to import new cards
+options_action = QAction(f"Reload 1", mw)
+options_action.triggered.connect(lambda _, o=mw: reload_addon())
+mw.form.menuTools.addAction(options_action)
 
 
-gui_hooks.profile_did_open.append(lambda *args: init())
+def reload_addon():
+    import_dict_cc.remove_menu_items()
+    importlib.reload(import_dict_cc)
+    # import import_dict_cc
+    import_dict_cc.add_menu_items()
---
+++ import_dict_cc/__init__.py
@@ -0,0 +1,75 @@
+import importlib
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+print(sys.path)
+
+import constants
+import edit_words_dialog
+import test
+import utils
+
+importlib.reload(constants)
+importlib.reload(edit_words_dialog)
+importlib.reload(test)
+importlib.reload(utils)
+
+importlib.reload(constants)
+
+import constants
+import edit_words_dialog
+import test
+import utils
+
+from aqt.qt import QAction
+from aqt import gui_hooks
+import importlib
+import os
+import sys
+from aqt import mw
+
+import_task = None
+print("loaded module")
+
+
+def start_import(profile: constants.Profile):
+    global import_task
+    import_task = profile.start_import()
+
+
+currently_added_actions: [QAction] = []
+
+
+def add_action_items(profile: constants.Profile):
+    # add menu option to import new cards
+    options_action = QAction(f"Import from Cambridge ({profile.language})...", mw)
+    options_action.triggered.connect(lambda _, o=mw: start_import(profile))
+    mw.form.menuTools.addAction(options_action)
+    currently_added_actions.append(options_action)
+
+    # add menu option to import new cards
+    options_action = QAction(f"Update Tampermonkey list ({profile.language})", mw)
+    options_action.triggered.connect(lambda _, o=mw: profile.update_tampermonkey_list())
+    mw.form.menuTools.addAction(options_action)
+    currently_added_actions.append(options_action)
+
+
+def remove_menu_items ():
+    global currently_added_actions
+    for action in currently_added_actions:
+        print("unloading")
+        mw.form.menuTools.removeAction(action)
+    currently_added_actions = []
+
+
+def add_menu_items():
+    # reload()
+    profiles: [constants.Profile] = [constants.EnglishProfile(), constants.FrenchProfile()]
+    for profile in profiles:
+        add_action_items(profile)
+
+    # add menu option to import new cards
+    options_action = QAction(f"Test", mw)
+    options_action.triggered.connect(lambda _, o=mw: test.test())
+    mw.form.menuTools.addAction(options_action)
+    currently_added_actions.append(options_action)
